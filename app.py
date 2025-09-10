329231627bded81fb833e0101d5c33ba3881efce
Pradeep <mail.pradeep.sharma@gmail.com>
2025-09-10 20:42:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -50,3 +50,8 @@ def webhook():
             print("Error:", e)
 
         return "ok", 200
+
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
