4629c7968b8c12a211038cc46d3008598b659f52
Pradeep <mail.pradeep.sharma@gmail.com>
2025-09-11 01:05:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 from twilio.twiml.messaging_response import MessagingResponse
+import os
 
 app = Flask(__name__)
 
@@ -16,7 +17,9 @@ def webhook():
     else:
         msg.body("Sorry, I didn't understand that.")
 
-    return str(resp)
+    return str(resp), 200  # Make sure to return status code 200
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Get Render-provided port
+    app.run(host="0.0.0.0", port=port, debug=True)  # Bind to all interfaces
